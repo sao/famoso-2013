787f86a9f5cd814dbfb37d588e6771492e96dbc3
Brian McCoy <brianm@cubicleninjas.com>
2014-03-05 19:04:48 -0600

    Updated site to be responsive.
    Signed-off-by: Brian McCoy <brianm@cubicleninjas.com>

--- js/global.js
+++ js/global.js
@@ -1,1 +1,32 @@
+$(function() {
 
+  var methods = {
+
+    init: function() {
+      this.mobile_menu_trigger();
+    },
+
+    // Handles when the "hamburger icon" is clicked.
+    mobile_menu_trigger: function() {
+      $('.mobile-nav-toggle').on('click', function(e) {
+        e.preventDefault();
+
+        var container_height = $('.header nav').height();
+
+        if(container_height == 0) {
+          $('.header nav').animate({
+            height: 300
+          });
+        } else {
+          $('.header nav').animate({
+            height: 0
+          });
+        }
+      });
+    }
+
+  };
+
+  methods.init();
+
+});
